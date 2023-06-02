1bd7ae334076662709ab03d6e666c744da3dba75
git-savio1.domi <techsavvydominic3@gmail.com>
2023-06-02 10:35:39 +0300

    Completed assignments

---
+++ lib/create.sql
@@ -0,0 +1,11 @@
+-- Active: 1685003562094@@127.0.0.1@3306
+CREATE TABLE bears (
+id INTEGER PRIMARY KEY,
+name TEXT,
+age INTEGER,
+sex TEXT,
+color TEXT,
+temperament TEXT,
+alive BOOLEAN
+-- temperament TEXT,
+)
---
+++ lib/insert.sql
@@ -0,0 +1,11 @@
+-- Active: 1685003562094@@127.0.0.1@3306
+INSERT INTO bears(name, age, sex, color, temperament, alive)
+VALUES
+    ("Mojab", 25, "M", "Brown", "Dangerous", true),
+    ("Diana", 18, "F", "White", "Friendly", true),
+    ("Gran", 56, "M", "Black", "Neutral", false),
+    ("Queen", 35, "F", "Brown", "Dangerous", true),
+    ("Maria", 17, "F", "Black", "Sleepy", true),
+    ("Bran", 87, "M", "Brown", "Neutral", false),
+    ("Bran", 35, "M", "Brown", "Dangerous", true),
+    (NULL, 47, "F", "White", "Slow", true)
